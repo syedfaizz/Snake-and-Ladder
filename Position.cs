9a89da00ccf181db5b7d33cf850955375c1a88ff
syedfaizz <syedfaizz45@gmail.com>
2021-08-01 13:00:53 +0530

    [faisal] Add: uc1-Position

---
+++ Position.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeLadderSimulation
+{
+    class SnakeLadderSimulator
+    {
+        // variable
+        int position = 0;
+
+        /// <summary>
+        /// Single Player starting postion is zero.
+        /// </summary>
+        public void start()
+        {
+            Console.WriteLine("Starting position is:" + position);
+        }
+    }
+}
